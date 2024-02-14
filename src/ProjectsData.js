3195bcc4ef7138cd63215df328fe629d29a9325d
anandafn <anandanamira03@gmail.com>
2024-02-14 16:10:52 +0700

    add styling and elements

---
+++ src/ProjectsData.js
@@ -0,0 +1,31 @@
+import projectImg1 from "./assets/images/project1.png";
+import projectImg2 from "./assets/images/project2.png";
+import projectImg3 from "./assets/images/project3.png";
+import projectImg4 from "./assets/images/project4.png";
+
+export const projects = [
+  {
+    title: "Omnifood",
+    description: "Healthy meal",
+    imgUrl: projectImg1,
+    url: "https://omnifood-web-portfolio.netlify.app/",
+  },
+  {
+    title: "Forkify",
+    description: "Recipes",
+    imgUrl: projectImg2,
+    url: "https://drive.google.com/file/d/1bNCx7Hzk4VN9tPZjAJwB3nJ1RDbw9dkG/view",
+  },
+  {
+    title: "Dashboard",
+    description: "UI Dashboard",
+    imgUrl: projectImg3,
+    url: "https://your-dashboard.netlify.app/",
+  },
+  {
+    title: "Travel",
+    description: "Booking",
+    imgUrl: projectImg4,
+    url: "https://drive.google.com/file/d/1lcwd3fmh8820GRn1ND7lOgVd2WFIwYoH/view",
+  },
+];
